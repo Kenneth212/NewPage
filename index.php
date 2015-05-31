9cfaf5ad30045b40882f6654d26cd75082166c75
Kenneth Prado <Kennethprado45@gmail.com>
2015-05-31 11:46:04 -0700

    NewPage added some new code.

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title> Kenneth Prado </title>
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+	<h1>Kenneth Prado | Web Developer</h1>
 </head>
 <body>
+<div id="nav">
+	<div id="nav_wrapper">
+		<ul>
+			<li><a href="#">Home</a></li><li>
+			<a href="#">About</a></li><li>
+			<a href="#">Contact</a></li><li>
+			<a href="#">Social</a>
+				<ul>
+					<li><a href="#">Youtube</a></li>
+					<li><a href="#">Twitter</a></li>
+					<li><a href="#">Facebook</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
 
 </body>
 </html>
