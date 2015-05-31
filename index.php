6ff713e9eac8efcef7985bcf1705787c61946771
Kenneth Prado <Kennethprado45@gmail.com>
2015-05-31 16:01:44 -0700

    NewPage added a carousel that for some reason just disappears

--- index.php
+++ index.php
@@ -2,16 +2,23 @@
 <html>
 <head>
 	<title> Kenneth Prado </title>
+	<link rel="icon" type="iamge/png" href="http://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Linea_K_(Logo_Metro_Medellin).svg/2000px-Linea_K_(Logo_Metro_Medellin).svg.png">
 	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<h1>Kenneth Prado | Web Developer</h1>
 </head>
 <body>
 <div id="nav">
 	<div id="nav_wrapper">
 		<ul>
-			<li><a href="#">Home</a></li><li>
+			<li><a href="http://localhost/NewPage/index.php">Home</a></li><li>
 			<a href="#">About</a></li><li>
-			<a href="#">Contact</a></li><li>
+			<a href="#">Contact</a>
+				<ul>
+					<li><a href="#">Google Email</a></li>
+				</ul>
+			</li><li>
 			<a href="#">Social</a>
 				<ul>
 					<li><a href="#">Youtube</a></li>
@@ -22,6 +29,25 @@
 		</ul>
 	</div>
 </div>
-
+<section>
+<div id="wrapper"><h2>
+<header>Projects</header>
+	<div class="instagram col-xs-6">
+		<a href="http://localhost/Kenneth/index.php">
+		<img alt="images/instagram" src="images/instagram.png" class="hover" width="550px" />
+		</a>
+		<a href="http://localhost/Kenneth/index.php">
+		<img alt="images/instagram" src="images/instagram.png" class="hover" width="550px" /></h2>
+		</a>
+	</div>
+</div>
+</section>
+<div class="css-carousel">
+	<img src="images/image1.jpg" class="css-img"/>
+	<img src="images/image2.jpg" class="css-img"/>
+	<img src="images/image3.jpg" class="css-img"/>
+	<img src="images/image4.jpg" class="css-img"/>
+	<img src="images/image5.jpg" class="css-img"/>
+</div>
 </body>
 </html>
